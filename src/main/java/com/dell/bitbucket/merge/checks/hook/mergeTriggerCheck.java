f5d80cfdb599b06476ed16be0b43dd3de014ae79
pgits <petergits@gmail.com>
2018-04-04 11:55:46 -0700

    This successfully worked on the development version 4.14.3 on my desktop, pushing it now to the stash production server... to see if it will trigger something.

--- src/main/java/com/dell/bitbucket/merge/checks/hook/mergeTriggerCheck.java
+++ src/main/java/com/dell/bitbucket/merge/checks/hook/mergeTriggerCheck.java
@@ -22,6 +22,7 @@ import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.lang.StringUtils;
 import java.util.StringJoiner;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
@@ -31,36 +32,149 @@ import org.slf4j.*;
 import java.util.regex.Pattern;
 import javax.annotation.Nonnull;
 
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.*;
+
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.regex.*;
+import java.util.Arrays;
+import java.util.regex.PatternSyntaxException;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 
 public class mergeTriggerCheck implements MergeRequestCheck {
 
 
     //private final I18nService i18nService;
     //private final PullRequestService pullRequestService;
+    final static Logger log = LoggerFactory.getLogger(mergeTriggerCheck.class);
 
     public mergeTriggerCheck(/*I18nService i18nService, PullRequestService pullRequestService*/) {
         //this.i18nService = i18nService;
         //this.pullRequestService = pullRequestService;
-        System.out.println("initialized mergeTriggerCheck");
+        log.error("initialized mergeTriggerCheck");
     }
 
     @Override
     public void check(@Nonnull MergeRequest request) {
-        System.out.println(String.format("MergeRequest check called:  repo name = %s", request.getPullRequest().getToRef().getRepository().getId()));
-        //TODO: implement me
+        log.error(String.format("MergeRequest check called:  repo name = %s", request.getPullRequest().getToRef().getRepository().getName()));
+
+
+        log.error("running com.dell.bitbucket.merge.checks.hook.mergeTriggerCheck:check");
+        MergeRequest mr        = request;
+        PullRequest pr         = mr.getPullRequest();
+        PullRequestRef prrFrom = pr.getFromRef();
+        PullRequestRef prrTo   = pr.getToRef();
+
+
+        Repository repository = prrTo.getRepository();
+        String url = "http://dockerlogin-eqx-01.force10networks.com:8080/mergeTrigger.php";
+        String branch = prrTo.getDisplayId();
+        String packageName = repository.getName();
+
+        String keyPackageName  = repository.getProject().getKey();
+        String fullPackageName = repository.getProject().getName();
+        //String packageRevision = "4.0.0.84";
+            if(!keyPackageName.toLowerCase().startsWith("ar")) {//this is not our repo, send it on its way
+            log.error("this packageName.key doesn't start with ar");
+            log.error(String.format("keyPackageName is equal to %s", keyPackageName.toLowerCase()));
+            log.error(String.format("RepositoryHookResult is now accepted"));
+            return ;//RepositoryHookResult.accepted();
+        }
+
+        long lpullRequestId = pr.getId();
+        String pullRequestId = String.valueOf(lpullRequestId);
+        String jsonContainer = String.format("description=%s", pr.getDescription());
+        String mergeUser = "Administrator";
+        //String newBranch = branch.replace("/", "!!!");
+        //char ampresand = '&';
+
+        String charset = "UTF-8";  // Or in Java 7 and later, use the constant: java.nio.charset.StandardCharsets.UTF_8.name()
+        try {
+
+            //URLConnection connection = new URL(url + "?" + query).openConnection();
+            URLConnection connection = new URL(url).openConnection();
+            HttpURLConnection http = (HttpURLConnection)connection;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            Map<String,String> arguments = new HashMap();
+            arguments.put("my_branch", branch);
+            arguments.put("packageName", packageName);
+            arguments.put("packageRevision", "-1");
+            arguments.put("jsonContainer", jsonContainer);
+            arguments.put("mergeUser", mergeUser);
+            arguments.put("pullRequestId", pullRequestId);
+
+            StringJoiner sj = new StringJoiner("&");
+            for(Map.Entry<String,String> entry : arguments.entrySet())
+                sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "="
+                        + URLEncoder.encode(entry.getValue(), "UTF-8"));
+            byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            http.setRequestProperty("Accept-Charset", charset);
+            http.connect();
+            try {
+                OutputStream os = http.getOutputStream();
+                os.write(out);
+            }catch(Exception ee){
+                ee.printStackTrace();
+            }
+
+            java.io.InputStream response = http.getInputStream();
+            java.io.BufferedReader reader = new java.io.BufferedReader(new InputStreamReader(response));
+            // And print each line
+            String s = null;
+            StringBuilder returnResults = new StringBuilder();
+            Boolean returnValue = false;
+            while ((s = reader.readLine()) != null) {
+                log.info(s);
+                returnResults.append(s);
+            }
+            http.disconnect();
+            if(returnResults.toString().contains("MergeCheckPassed")) {
+                if (returnResults.toString().contains("succeeded")) {
+                    log.error("returning success for merge, found MergeCheckPassed + succeeded");
+                    return;//RepositoryHookResult.accepted();
+                }
+            }
+        }catch(MalformedURLException me){
+            me.printStackTrace();
+            //handle the exception if the target service is down, for now allow it to pass through
+            //eventually don't
+            log.error("malformedURLException: returning success for merge");
+            return ;//RepositoryHookResult.accepted();
+        }catch(Exception io){
+            io.printStackTrace();
+            //handle the exception if the target service is down, for now allow it to pass through
+            //eventually don't
+            log.error("ioException: returning success for merge");
+            return ;//RepositoryHookResult.accepted();
+        }
         /*
-        String summaryMsg = i18nService.getText("com.dell.bitbucket.merge.check.summary",
-                "Only repository administrators may approve pull requests");
-        String detailedMsg = i18nService.getText("com.dell.bitbucket.merge.check.detailed",
-                "The user merging the pull request must be an administrator of the target repository");
-        request.veto(summaryMsg, detailedMsg);
-        */
         String summaryMsg = "com.dell.bitbucket.merge.check.summary:  " +
                 "Only repository administrators may approve pull requests";
+        //request.veto(summaryMsg, detailedMsg);
+        */
+
+        String summaryMsg  = "checking dell.engOps implementation";
         String detailedMsg = "com.dell.bitbucket.merge.check.detailed:  " +
-                "The user merging the pull request must be an administrator of the target repository";
-        request.veto(summaryMsg, detailedMsg);
+                "The pull request must first pass the Continuum: Merge Trigger Build & the Merge Trigger Smoke Test prior to merging ";
+        mr.veto (summaryMsg, detailedMsg);
+        log.error(String.format("returning veto: %s:%s", summaryMsg, detailedMsg));
     }
+
 }
 
 
